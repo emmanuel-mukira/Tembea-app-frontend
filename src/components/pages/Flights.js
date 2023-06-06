9c8084c50cae29264206bc9dbaf741fe4e7f79c7
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-06-06 11:34:45 +0300

    Fetching from backend

--- src/components/pages/Flights.js
+++ src/components/pages/Flights.js
@@ -1,6 +1,39 @@
-import React from 'react';
-import '../../App.css';
+import React, { useEffect, useState } from 'react';
+import CardItem from '../CardItem';
+import '../Cards.css';
 
-export default function Flights() {
-  return <h1 className='flights'>FLIGHTS</h1>;
+function Flights() {
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    // Fetch flights data from the backend API
+    fetch('http://localhost:9292/flights')
+      .then(response => response.json())
+      .then(data => setFlights(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Flights</h1>
+      <div className="cards">
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              {flights.map(flight => (
+                <CardItem
+                  key={flight.id}
+                  src={flight.image_url}
+                  text={flight.flight_number}
+                  label={flight.status}
+                  path={`/flights/${flight.id}`} // Assuming a specific flight details page
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Flights;
--- src/components/pages/Hotels.js
+++ src/components/pages/Hotels.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import '../../App.css';
+import React, { useEffect, useState } from 'react';
+import CardItem from '../CardItem';
+import '../Cards.css';
 
-export default function Hotels() {
-  return <h1 className='hotels'>HOTELS</h1>;
+
+function Hotels() {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    // Fetch hotels data from the backend API
+    fetch('http://localhost:9292/hotels')
+      .then(response => response.json())
+      .then(data => setHotels(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hotels</h1>
+      <div className="cards">
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              {hotels.map(hotel => (
+                <CardItem
+                  key={hotel.id}
+                  src={hotel.image_url}
+                  text={hotel.name}
+                  label={hotel.star_rating}
+                  path={`/hotels/${hotel.id}`} // Assuming a specific hotel details page
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Hotels;
